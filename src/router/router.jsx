d48fe77cf5ce4717c1bf5369a93abe7f97c4c520
MursalinDevOps <mursalin1768@gmail.com>
2024-12-28 16:09:30 +0600

    navbar added

--- src/router/router.jsx
+++ src/router/router.jsx
@@ -2,11 +2,20 @@ import {
     createBrowserRouter,
     RouterProvider,
   } from "react-router-dom";
+import MainLayout from "../layouts/MainLayout";
+import Home from "../pages/home/Home";
 
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <div>Hello world!</div>,
+      element: <MainLayout></MainLayout>,
+      errorElement:<div>404, Route not found!</div>,
+      children: [
+        {
+            path: '/',
+            element: <Home></Home>
+        },
+      ]
     },
   ]);
 
